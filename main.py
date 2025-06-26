01dd237ce9928daf0416d91cf10ed1808ce4e1ea
saranbabu698 <saranbabu698@gmail.com>
2025-06-26 21:25:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ print("sum:", sum(nums))
 print("    fsum:", fsum(nums))
 print ("Thankyou!!") #RAW CODE
 print ("Thankyou!!") #d1 branch changes+c1 branch changes
+print ("Thankyou!!") #y1 branch changes - stash demo
 
 
 
